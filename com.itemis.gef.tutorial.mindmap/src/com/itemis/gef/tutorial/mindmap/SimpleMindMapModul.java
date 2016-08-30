2fcef9771ee24312c244b55ec45f483823db36db
Hannes Niederhausen <hniederhausen@itemis.de>
2016-08-30 11:22:32 +0200

    Step 5 - added selection and hover policies

--- com.itemis.gef.tutorial.mindmap/src/com/itemis/gef/tutorial/mindmap/SimpleMindMapModul.java
+++ com.itemis.gef.tutorial.mindmap/src/com/itemis/gef/tutorial/mindmap/SimpleMindMapModul.java
@@ -3,6 +3,12 @@ package com.itemis.gef.tutorial.mindmap;
 import org.eclipse.gef.common.adapt.AdapterKey;
 import org.eclipse.gef.common.adapt.inject.AdapterMaps;
 import org.eclipse.gef.mvc.fx.MvcFxModule;
+import org.eclipse.gef.mvc.fx.behaviors.FXHoverBehavior;
+import org.eclipse.gef.mvc.fx.parts.FXDefaultHoverFeedbackPartFactory;
+import org.eclipse.gef.mvc.fx.parts.FXDefaultSelectionFeedbackPartFactory;
+import org.eclipse.gef.mvc.fx.policies.FXFocusAndSelectOnClickPolicy;
+import org.eclipse.gef.mvc.fx.policies.FXHoverOnHoverPolicy;
+import org.eclipse.gef.mvc.fx.providers.ShapeBoundsProvider;
 import org.eclipse.gef.mvc.fx.providers.ShapeOutlineProvider;
 import org.eclipse.gef.mvc.parts.IContentPartFactory;
 
@@ -13,7 +19,6 @@ import com.itemis.gef.tutorial.mindmap.parts.MindMapPartsFactory;
 import com.itemis.gef.tutorial.mindmap.parts.SimpleMindMapAnchorProvider;
 
 import javafx.scene.Node;
-
 
 /**
  * 
@@ -28,28 +33,59 @@ public class SimpleMindMapModul extends MvcFxModule {
 	protected void configure() {
 		// start the default configuration
 		super.configure();
-		
+
 		bindMindMapNodePartAdapters(AdapterMaps.getAdapterMapBinder(binder(), MindMapNodePart.class));
 	}
-	
+
 	/**
 	 * 
 	 * @param adapterMapBinder
 	 */
 	protected void bindMindMapNodePartAdapters(MapBinder<AdapterKey<?>, Object> adapterMapBinder) {
-		// bind anchor provider used to create the connection anchors 
+		// bind anchor provider used to create the connection anchors
 		adapterMapBinder.addBinding(AdapterKey.defaultRole()).to(SimpleMindMapAnchorProvider.class);
-		
+
 		// bind a geometry provider, which is used in our anchor provider
 		adapterMapBinder.addBinding(AdapterKey.defaultRole()).to(ShapeOutlineProvider.class);
-		
+
+		// provides a hover feedback to the shape, , used by the FXHoverBehavior
+		AdapterKey<?> role = AdapterKey.role(FXDefaultHoverFeedbackPartFactory.HOVER_FEEDBACK_GEOMETRY_PROVIDER);
+		adapterMapBinder.addBinding(role).to(ShapeOutlineProvider.class);
+
+		// provides a selection feedback to the shape
+		role = AdapterKey.role(FXDefaultSelectionFeedbackPartFactory.SELECTION_FEEDBACK_GEOMETRY_PROVIDER);
+		adapterMapBinder.addBinding(role).to(ShapeBoundsProvider.class);
 	}
 
 	@Override
 	protected void bindIContentPartFactoryAsContentViewerAdapter(MapBinder<AdapterKey<?>, Object> adapterMapBinder) {
 		super.bindIContentPartFactoryAsContentViewerAdapter(adapterMapBinder);
-		
-		// binding one instance of our factory to the IContentPartFactory type, to be used to create our parts
-		binder().bind(new TypeLiteral<IContentPartFactory<Node>>() {}).toInstance(new MindMapPartsFactory());
+
+		// binding one instance of our factory to the IContentPartFactory type,
+		// to be used to create our parts
+		binder().bind(new TypeLiteral<IContentPartFactory<Node>>() {
+		}).toInstance(new MindMapPartsFactory());
+	}
+
+	@Override
+	protected void bindContentViewerRootPartAdapters(MapBinder<AdapterKey<?>, Object> adapterMapBinder) {
+		super.bindContentViewerRootPartAdapters(adapterMapBinder);
+
+		// binding a hover behavior to the root part. it will react to
+		// HoverModel changes and render the hover part
+		adapterMapBinder.addBinding(AdapterKey.defaultRole()).to(FXHoverBehavior.class);
+	}
+
+	@Override
+	protected void bindAbstractContentPartAdapters(MapBinder<AdapterKey<?>, Object> adapterMapBinder) {
+		super.bindAbstractContentPartAdapters(adapterMapBinder);
+
+		// binding the FXHoverOnHoverPolicy to every part
+		// if a mouse is moving above a part it is set i the HoverModel
+		adapterMapBinder.addBinding(AdapterKey.defaultRole()).to(FXHoverOnHoverPolicy.class);
+
+		// add the focus and select policy to every part, listening to clicks
+		// and changing the focus and selection model
+		adapterMapBinder.addBinding(AdapterKey.defaultRole()).to(FXFocusAndSelectOnClickPolicy.class);
 	}
 }
