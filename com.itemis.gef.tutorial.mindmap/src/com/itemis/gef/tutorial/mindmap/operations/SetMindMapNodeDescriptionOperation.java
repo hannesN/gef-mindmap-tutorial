744efe374d87ad4e2d5012a3cf596236e47f0f2b
Hannes Niederhausen <hniederhausen@itemis.de>
2016-08-31 11:53:47 +0200

    step 11 - modifying nodes

---
+++ com.itemis.gef.tutorial.mindmap/src/com/itemis/gef/tutorial/mindmap/operations/SetMindMapNodeDescriptionOperation.java
@@ -0,0 +1,62 @@
+package com.itemis.gef.tutorial.mindmap.operations;
+
+import org.eclipse.core.commands.ExecutionException;
+import org.eclipse.core.commands.operations.AbstractOperation;
+import org.eclipse.core.runtime.IAdaptable;
+import org.eclipse.core.runtime.IProgressMonitor;
+import org.eclipse.core.runtime.IStatus;
+import org.eclipse.core.runtime.Status;
+import org.eclipse.gef.mvc.operations.ITransactionalOperation;
+
+import com.itemis.gef.tutorial.mindmap.parts.MindMapNodePart;
+
+/**
+ * operation to change the description property of a MindMapNode
+ * 
+ * @author hniederhausen
+ *
+ */
+public class SetMindMapNodeDescriptionOperation extends AbstractOperation implements ITransactionalOperation {
+
+	private final MindMapNodePart nodePart;
+	private final String oldDescription;
+	private final String newDescription;
+
+	public SetMindMapNodeDescriptionOperation(MindMapNodePart nodePart, String newDescription) {
+		super("Change color");
+		this.nodePart = nodePart;
+		this.newDescription = newDescription;
+		this.oldDescription = nodePart.getContent().getDescription();
+	}
+
+	@Override
+	public boolean isContentRelevant() {
+		// yes we change the model
+		return true;
+	}
+
+	@Override
+	public boolean isNoOp() {
+		return newDescription.equals(oldDescription);
+	}
+
+	@Override
+	public IStatus execute(IProgressMonitor monitor, IAdaptable info) throws ExecutionException {
+		nodePart.getContent().setDescription(newDescription);
+		nodePart.refreshVisual();
+		return Status.OK_STATUS;
+	}
+
+	@Override
+	public IStatus redo(IProgressMonitor monitor, IAdaptable info) throws ExecutionException {
+		return execute(monitor, info);
+	}
+
+	@Override
+	public IStatus undo(IProgressMonitor monitor, IAdaptable info) throws ExecutionException {
+		nodePart.getContent().setDescription(oldDescription);
+		nodePart.refreshVisual();
+		return Status.OK_STATUS;
+	}
+
+}
--- com.itemis.gef.tutorial.mindmap/src/com/itemis/gef/tutorial/mindmap/policies/ShowMindMapNodeContextMenuOnClickPolicy.java
+++ com.itemis.gef.tutorial.mindmap/src/com/itemis/gef/tutorial/mindmap/policies/ShowMindMapNodeContextMenuOnClickPolicy.java
@@ -1,19 +1,29 @@
 package com.itemis.gef.tutorial.mindmap.policies;
 
+import java.util.Optional;
+
 import org.eclipse.core.commands.ExecutionException;
 import org.eclipse.gef.mvc.fx.policies.IFXOnClickPolicy;
+import org.eclipse.gef.mvc.operations.ITransactionalOperation;
 import org.eclipse.gef.mvc.parts.IVisualPart;
 import org.eclipse.gef.mvc.policies.AbstractInteractionPolicy;
 import org.eclipse.gef.mvc.viewer.IViewer;
 
 import com.itemis.gef.tutorial.mindmap.operations.DeleteNodeOperation;
+import com.itemis.gef.tutorial.mindmap.operations.SetMindMapNodeColorOperation;
+import com.itemis.gef.tutorial.mindmap.operations.SetMindMapNodeDescriptionOperation;
+import com.itemis.gef.tutorial.mindmap.operations.SetMindMapNodeTitleOperation;
 import com.itemis.gef.tutorial.mindmap.parts.MindMapNodePart;
 import com.itemis.gef.tutorial.mindmap.parts.SimpleMindMapPart;
 
 import javafx.scene.Node;
 import javafx.scene.control.ContextMenu;
+import javafx.scene.control.Menu;
 import javafx.scene.control.MenuItem;
+import javafx.scene.control.TextInputDialog;
 import javafx.scene.input.MouseEvent;
+import javafx.scene.paint.Color;
+import javafx.scene.shape.Rectangle;
 
 /**
  * This policy shows a context menu for MindMapNodeParts, providing some editing functionality.
@@ -46,8 +56,96 @@ public class ShowMindMapNodeContextMenuOnClickPolicy extends AbstractInteraction
 			}
 		});
 		
-		ContextMenu ctxMenu = new ContextMenu(deleteNodeItem);
+		
+		Menu textMenu = createChangeTextsMenu();
+		Menu colorMenu = createChangeColorMenu();
+		
+		ContextMenu ctxMenu = new ContextMenu(textMenu, colorMenu, deleteNodeItem);
 		// show the menu at the mouse position
 		ctxMenu.show((Node) event.getTarget(), event.getScreenX(), event.getScreenY());
 	}
+
+	private Menu createChangeTextsMenu() {
+		Menu textsMenu = new Menu("Change");
+		
+		MindMapNodePart host = (MindMapNodePart) getHost();
+		
+		MenuItem titleItem = new MenuItem("Title ...");
+		titleItem.setOnAction((e) -> {
+			try {
+				String newTitle = showDialog(host.getContent().getTitle(), "Enter new Title...");
+				ITransactionalOperation op = new SetMindMapNodeTitleOperation(host, newTitle);
+				host.getRoot().getViewer().getDomain().execute(op, null);
+			} catch (ExecutionException e1) {
+				e1.printStackTrace();
+			}
+			
+		});
+		
+		MenuItem descrItem = new MenuItem("Description ...");
+		descrItem.setOnAction((e) -> {
+			try {
+				String newDescription = showDialog(host.getContent().getDescription(), "Enter new Description...");
+				ITransactionalOperation op = new SetMindMapNodeDescriptionOperation(host, newDescription);
+				host.getRoot().getViewer().getDomain().execute(op, null);
+			} catch (ExecutionException e1) {
+				e1.printStackTrace();
+			}
+		});
+		
+		textsMenu.getItems().addAll(titleItem, descrItem);
+		
+		return textsMenu;
+	}
+
+	private String showDialog(String defaultValue, String title) {
+		TextInputDialog dialog = new TextInputDialog(defaultValue);
+		dialog.setTitle(title);
+		dialog.setGraphic(null);
+		dialog.setHeaderText("");
+		
+		Optional<String> result = dialog.showAndWait();
+		String entered = defaultValue;
+	
+		if (result.isPresent()) {
+	
+		    entered = result.get();
+		}
+		return entered;
+	}
+
+	private Menu createChangeColorMenu() {
+		Menu colorMenu = new Menu("Change Color");
+		Color[] colors = {Color.ALICEBLUE, Color.BURLYWOOD, Color.YELLOW, Color.RED, Color.CHOCOLATE, Color.GREENYELLOW, Color.WHITE};
+		String[] names = {"ALICEBLUE", "BURLYWOOD", "YELLOW", "RED", "CHOCOLATE", "GREENYELLOW", "WHITE"};
+		
+		for (int i=0; i<colors.length; i++) {
+			colorMenu.getItems().add(getColorMenuItem(names[i], colors[i]));
+		}
+		return colorMenu;
+	}
+	
+	private MenuItem getColorMenuItem(String name, Color color) {
+		Rectangle graphic = new Rectangle(20, 20);
+		graphic.setFill(color);
+		graphic.setStroke(Color.BLACK);
+		MenuItem item = new MenuItem(name, graphic);
+		item.setOnAction((e) -> submitColor(color));
+		return item;
+	}
+	
+	private void submitColor(Color color) {
+		if (getHost() instanceof MindMapNodePart) {
+			MindMapNodePart host = (MindMapNodePart) getHost();
+			
+			SetMindMapNodeColorOperation op = new SetMindMapNodeColorOperation(host, color);
+			
+			try {
+				host.getRoot().getViewer().getDomain().execute(op, null);
+			} catch (ExecutionException e) {
+				e.printStackTrace();
+			}
+		}
+	}
+	
 }
