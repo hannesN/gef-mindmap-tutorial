b1eac8c8c6b36e13b24e3a64718718436eb78f13
hniederhausen <hniederhausen@itemis.de>
2016-09-28 11:08:01 +0200

    Removed CreateConnectionOperation and using GEFs CreatePolicy

--- com.itemis.gef.tutorial.mindmap/src/com/itemis/gef/tutorial/mindmap/policies/CreateNewConnectiononClickPolicy.java
+++ com.itemis.gef.tutorial.mindmap/src/com/itemis/gef/tutorial/mindmap/policies/CreateNewConnectiononClickPolicy.java
@@ -1,14 +1,18 @@
 package com.itemis.gef.tutorial.mindmap.policies;
 
-import org.eclipse.core.commands.ExecutionException;
 import org.eclipse.gef.mvc.fx.policies.IFXOnClickPolicy;
+import org.eclipse.gef.mvc.parts.IContentPart;
+import org.eclipse.gef.mvc.parts.IRootPart;
 import org.eclipse.gef.mvc.parts.IVisualPart;
 import org.eclipse.gef.mvc.policies.AbstractInteractionPolicy;
+import org.eclipse.gef.mvc.policies.CreationPolicy;
 import org.eclipse.gef.mvc.viewer.IViewer;
 
+import com.google.common.collect.HashMultimap;
+import com.google.common.reflect.TypeToken;
+import com.itemis.gef.tutorial.mindmap.model.MindMapConnection;
 import com.itemis.gef.tutorial.mindmap.models.ItemCreationModel;
 import com.itemis.gef.tutorial.mindmap.models.ItemCreationModel.Type;
-import com.itemis.gef.tutorial.mindmap.operations.CreateConnectionOperation;
 import com.itemis.gef.tutorial.mindmap.parts.MindMapNodePart;
 import com.itemis.gef.tutorial.mindmap.parts.SimpleMindMapPart;
 
@@ -17,6 +21,7 @@ import javafx.scene.input.MouseEvent;
 
 public class CreateNewConnectiononClickPolicy extends AbstractInteractionPolicy<Node> implements IFXOnClickPolicy {
 
+	@SuppressWarnings("serial")
 	@Override
 	public void click(MouseEvent e) {
 		
@@ -43,12 +48,25 @@ public class CreateNewConnectiononClickPolicy extends AbstractInteractionPolicy<
 		IVisualPart<Node, ? extends Node> part = getHost().getRoot().getChildrenUnmodifiable().get(0);
 		
 		if (part instanceof SimpleMindMapPart) {
-			CreateConnectionOperation op = new CreateConnectionOperation((SimpleMindMapPart) part, source, target);
-			try {
-				viewer.getDomain().execute(op, null);
-			} catch (ExecutionException e1) {
-				e1.printStackTrace();
-			}
+			
+			MindMapConnection newConn = new MindMapConnection();
+			newConn.connect(source.getContent(), target.getContent());
+
+			// GEF provides the CreatePolicy and operations to add a new element
+			// to the model
+			IRootPart<Node, ? extends Node> root = getHost().getRoot();
+			// get the policy bound to the IRootPart
+			CreationPolicy<Node> creationPolicy = root.getAdapter(new TypeToken<CreationPolicy<Node>>() {
+			});
+			// initialize the policy
+			init(creationPolicy);
+			// create a IContentPart for our new model. We don't use the
+			// returned content-part
+			creationPolicy.create(newConn, (SimpleMindMapPart) part,
+					HashMultimap.<IContentPart<Node, ? extends Node>, String>create());
+			// execute the creation
+			commit(creationPolicy);
+			
 		}
 		
 		// finally reset creationModel
