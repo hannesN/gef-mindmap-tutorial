d02228baf6d74bc71a050f5b89aa23e4ad861dcf
hniederhausen <hniederhausen@itemis.de>
2016-10-21 08:49:38 +0200

    Bugfix: Using DeletionPolicy works now
    Use AbstractInteractionPolicy.commit to commit the deletion on the domain, so the resulting operation is added to the history.
    Do not use DeletePolicy.commit

--- com.itemis.gef.tutorial.mindmap/src/com/itemis/gef/tutorial/mindmap/policies/ShowMindMapNodeContextMenuOnClickPolicy.java
+++ com.itemis.gef.tutorial.mindmap/src/com/itemis/gef/tutorial/mindmap/policies/ShowMindMapNodeContextMenuOnClickPolicy.java
@@ -1,19 +1,14 @@
 package com.itemis.gef.tutorial.mindmap.policies;
 
-import java.util.List;
+import java.util.ArrayList;
 
-import org.eclipse.gef.common.collections.ObservableMultiset;
 import org.eclipse.gef.mvc.fx.policies.IFXOnClickPolicy;
 import org.eclipse.gef.mvc.parts.IContentPart;
 import org.eclipse.gef.mvc.parts.IRootPart;
 import org.eclipse.gef.mvc.parts.IVisualPart;
 import org.eclipse.gef.mvc.policies.AbstractInteractionPolicy;
 import org.eclipse.gef.mvc.policies.DeletionPolicy;
-import org.eclipse.gef.mvc.viewer.IViewer;
 
-import com.google.common.collect.Lists;
-import com.google.common.collect.Multiset;
-import com.google.common.collect.Multisets;
 import com.google.common.reflect.TypeToken;
 import com.itemis.gef.tutorial.mindmap.parts.MindMapConnectionPart;
 
@@ -41,23 +36,19 @@ public class ShowMindMapNodeContextMenuOnClickPolicy extends AbstractInteraction
 		deleteNodeItem.setOnAction((e) -> {
 			IRootPart<Node, ? extends Node> root = getHost().getRoot();
 			DeletionPolicy<Node> delPolicy = root.getAdapter(new TypeToken<DeletionPolicy<Node>>() {});
-			delPolicy.init();
+			init(delPolicy);
 			
 			// get all achoreds and check if we have a connection part
-			List<MindMapConnectionPart> connectionPartList = Lists.newArrayList();
-			for (IVisualPart<Node, ? extends Node> a : getHost().getAnchoredsUnmodifiable())  {
+			for (IVisualPart<Node, ? extends Node> a : new ArrayList<>(getHost().getAnchoredsUnmodifiable()))  {
 				if (a instanceof MindMapConnectionPart) {
-					connectionPartList.add((MindMapConnectionPart) a);
+					// now delete the parts (couldn't do it before, because of a concurrent modification exception)
+					delPolicy.delete((IContentPart<Node, ? extends Node>) a);
 				}
 			}
-			// now delete the parts (couldn't do it before, because of a concurrent modification exception)
-			connectionPartList.forEach((c)->{
-				delPolicy.delete(c);
-			});
 			
 			// and finally remove the node part
 			delPolicy.delete((IContentPart<Node, ? extends Node>) getHost());
-			delPolicy.commit();
+			commit(delPolicy);
 		});
 		
 		ContextMenu ctxMenu = new ContextMenu(deleteNodeItem);
